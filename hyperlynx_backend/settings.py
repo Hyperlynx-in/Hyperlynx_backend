19bfb3a45ed3881de716b7d11c547312772f247d
Addy0000 <adarsht3322@gmail.com>
2026-02-04 23:36:58 +0530

    chnages for render

--- hyperlynx_backend/settings.py
+++ hyperlynx_backend/settings.py
@@ -34,7 +34,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1,.railway.app',
+    default='localhost,127.0.0.1,.railway.app,.onrender.com',
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 
